f48259a77a2a75f077fea7dd6718184cc68fc181
91986 <sanarmoor@gmail.com>
2023-09-03 22:51:51 +0530

    commit

---
+++ src/screens/PhoneAuth.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PhoneAuth = () => {
+  return (
+    <View>
+      <Text>PhoneAuth</Text>
+    </View>
+  )
+}
+
+export default PhoneAuth
