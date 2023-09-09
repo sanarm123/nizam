93d957ef9a8d690c3233861f3e8b471c8c3537d3
91986 <sanarmoor@gmail.com>
2023-09-10 00:47:16 +0530

    commit

--- src/screens/Notifications.js
+++ src/screens/Notifications.js
@@ -1,21 +1,18 @@
-import React from "react";
-import { View, Text,Dimensions, Image, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text,Dimensions, Image, StyleSheet,Button} from "react-native";
 import ImageZoom from 'react-native-image-pan-zoom';
 
 export default function Notifications() {
+  
+
   return (
     <View style={styles.container}>
-      <ImageZoom cropWidth={Dimensions.get('window').width}
-                       cropHeight={Dimensions.get('window').height}
-                       imageWidth={200}
-                       imageHeight={200}>
-                <Image style={{width:200, height:200}}
-                       source={{uri:'http://v1.qzone.cc/avatar/201407/07/00/24/53b9782c444ca987.jpg!200x200.jpg'}}/>
-            </ImageZoom>
+    
       <Text style={styles.title}>Notifications :)</Text>
       <Text style={styles.sub}>
         test
       </Text>
+
     </View>
   );
 }
