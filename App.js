149b86303f2f22ccd5eed43e7f7a9e46807e6651
91986 <sanarmoor@gmail.com>
2023-09-17 18:14:39 +0530

    commit

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { StyleSheet, Text, View ,Alert,Platform,Linking} from 'react-native';
 import Routes from './src/router'
 import 'react-native-gesture-handler';
 import { RootSiblingParent } from 'react-native-root-siblings';
+import registerNNPushToken from 'native-notify';
 
 import { useEffect,useRef } from 'react';
 
 
 export default function App() {
 
- 
+  registerNNPushToken(12183, 'Dl5jHPEE4JqTdgV3KjTMOh');
+  
   return (
     <>
     <RootSiblingParent>
