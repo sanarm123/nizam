8f469e2b949219026c6c711f2bb8cee20a4a657a
91986 <sanarmoor@gmail.com>
2023-10-21 10:22:25 +0530

    commit

--- functions/index.js
+++ functions/index.js
@@ -5,38 +5,15 @@ import {OpenAI} from 'openai'
 import { translate } from '@vitalets/google-translate-api';
 
 //Chat gpt key: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
+//bar api key :AIzaSyDd04i58X-gQ9opKzKwrV7y_fngLNZKitQ
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
+//"https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=YOUR_API_KEY"
 
 export const helloWorld = functions.https.onRequest((request, response) => {
 
-      const client = new TextToSpeechClient();
-
-      fetch('https://www.theguardian.com/uk/london/rss')
-      .then((responseData) =>{
-      // const data= responseData.rss.channel;
-        //const channel = rssParser.parse(responseData.rss);
-
-        const items = []
-        const item = {
-          title: 'Santosh Armoor',
-          description: 'test description',
-          date: '10/08/2023',
-          creator:'Reddy'
-          }
-
-          items.push(item)
-
-      // const itemsdata=JSON.parse(JSON.stringify(data._j)).items;
-          response.status(201)
-          .type('application/json')
-          .send(items);
-
-      })
-      .then((rss) => {
-      // console.log(rss.title);
-        response.send(rss); 
-    });
+  let name = request.body.name || request.query.name;
+  response.status(200).send(`Hello ${name}!`);
   
 });
 
@@ -113,7 +90,10 @@ export const ChatService = functions.https.onRequest(async (request, response) =
 
 export const ConvertTeluguToEnglish = functions.https.onRequest(async (request, response) => {
   
-  const { text } = await translate('The current Chief Minister of Telangana is K. Chandrashekar Rao, also known as KCR. He is the founder and president of the Telangana Rashtra Samithi (TRS) party. He was elected as the first Chief Minister of Telangana in 2014 and was re-elected in 2018. KCR was born in Karimnagar district of Telangana in 1952. He started his political career as a member of the Indian National Congress (INC) in 1982. He served as the Minister of Irrigation and Power in the INC government of Andhra Pradesh from 1984 to 1989. He left the INC in 1989 and formed the TRS party. The TRS party campaigned for the creation of a separate Telangana state. In 2014, the TRS won the Telangana Assembly elections and KCR became the first Chief Minister of Telangana. He has been re-elected as the Chief Minister of Telangana in 2018.KCR is a controversial figure. He has been accused of corruption and nepotism. However, he is also popular among the people of Telangana for his development work. He is known for his aggressive style of politics and his populist schemes. ', { to: 'te' });
+  
+  let description = request.body.text || request.query.text;
+
+  const { text } = await translate(description, { to: 'en' });
 
 
   try {
